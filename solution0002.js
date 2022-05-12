c8f84515a25c3dc6b416992c3f3a77e17f0ac4ee
eyejabb <eyejabb@gmail.com>
2022-05-12 14:29:53 -0700

    solution for split string into array

--- solution0002.js
+++ solution0002.js
@@ -1,13 +1,13 @@
-function removeExclamationMarks(s) {
-  var result = "";
-  var arrStr = s.split("");
-  for (var i = 0; i < arrStr.length; i++) {
-    if (arrStr[i] == "!") {
-      arrStr[i] = '';
-    }
-  }
-  for (var j = 0; j < arrStr.length; j++) {
-    result += arrStr[j];
-  }
-  return result;
-}
+// function removeExclamationMarks(s) {
+//   var result = "";
+//   var arrStr = s.split("");
+//   for (var i = 0; i < arrStr.length; i++) {
+//     if (arrStr[i] == "!") {
+//       arrStr[i] = '';
+//     }
+//   }
+//   for (var j = 0; j < arrStr.length; j++) {
+//     result += arrStr[j];
+//   }
+//   return result;
+// }
---
+++ solution0003.js
@@ -0,0 +1,7 @@
+function stringToArray(string){
+  let wordArray = string.split(" ")
+  return wordArray
+	// code code code
+
+}
+stringToArray("My name is Steven.")
