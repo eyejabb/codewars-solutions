9fcc2387b903dab1d7a0e80dd92232110c10abea
eyejabb <eyejabb@gmail.com>
2022-05-18 11:42:50 -0700

    return input parameter in string solution

---
+++ solution0009.js
@@ -0,0 +1,15 @@
+// const fetchPromise = fetch('https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json');
+
+// console.log(fetchPromise);
+
+// fetchPromise.then( response => {
+//   console.log(`Received response: ${response.status}`);
+// });
+
+// console.log("Started request...");
+
+
+function greet(name){
+  if(name === true)
+  return(`Hello, ${name} how are you doing today?`)
+}
