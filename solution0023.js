8ccb6e1d4415a8e5c9318c914f46510ba87dbeac
Steven Fitzgerald <eyejabb@gmail.com>
2022-05-31 11:16:43 -0700

    updated solution to Growth of a Population kata

--- solution0023.js
+++ solution0023.js
@@ -3,11 +3,9 @@
 // its population greater or equal to p = 1200 inhabitants?
 
 function nbYear(p0, percent, aug, p) {
-
-    var i = 0;
-    while (true) {
-      if (p0 >= p) {return i;}
-      p0 = p0 * (1 + percent/100) + aug; i++;
+    
+    for (var years = 0; p0 < p; years++) {
+      p0 = Math.floor(p0 + p0 * percent / 100 + aug);
     }
+    return years
   }
-  nbYear(1000, 2, 50, 1200)
