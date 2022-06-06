081415dfe89f23ed21f19ceaf2a1e92c6c28dbe3
Steven Fitzgerald <eyejabb@gmail.com>
2022-06-06 12:13:59 -0700

    Added best practice solution for 'Function 1 - hello world'

---
+++ Completed Solutions/solution0027.js
@@ -0,0 +1,19 @@
+// Description:
+
+// Make a simple function called greet that returns the most-famous "hello world!".
+
+// function greetWorld(){
+//     let gimmeanswer = "hello world"
+//     return gimmeanswer
+// }
+
+
+function greet(){
+    let gimmeanswer = "hello world!"
+    return gimmeanswer
+}
+
+//Best Practice//
+function greet(){
+    return "hello world!";
+}
