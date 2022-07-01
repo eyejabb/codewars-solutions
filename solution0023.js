d438d78d8ba9d8d3bca45394d8332313c86c72c3
Steven Fitzgerald <eyejabb@gmail.com>
2022-07-01 10:31:41 -0700

    testing contributions

---
+++ solution0023.js
@@ -0,0 +1,9 @@
+// function smash (words) {
+//     return words.join(' ');
+// }
+// function smash (words) {
+//     return words.join(' ');
+// }
+// function smash (words) {
+//     return words.join(' ');
+// }
