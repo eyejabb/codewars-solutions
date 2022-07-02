ddb79d9b60dea0a5843dea3d3f69bd1900da0641
Steven Fitzgerald <eyejabb@gmail.com>
2022-07-02 11:12:21 -0700

    solution to check how many years for one variable to change to another with additional equation to determine population

--- solution0023.js
+++ solution0023.js
@@ -1,9 +1,13 @@
-// function smash (words) {
-//     return words.join(' ');
-// }
-// function smash (words) {
-//     return words.join(' ');
-// }
-// function smash (words) {
-//     return words.join(' ');
-// }
+// In a small town the population is p0 = 1000 at the beginning of a year. The population regularly increases by 2 percent 
+// per year and moreover 50 new inhabitants per year come to live in the town. How many years does the town need to see 
+// its population greater or equal to p = 1200 inhabitants?
+
+function nbYear(p0, percent, aug, p) {
+
+    var i = 0;
+    while (true) {
+      if (p0 >= p) {return i;}
+      p0 = p0 * (1 + percent/100) + aug; i++;
+    }
+  }
+  nbYear(1000, 2, 50, 1200)
