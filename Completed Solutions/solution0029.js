e8779fce18cb7a61f7402b6389b1f2871e2e2a5f
Steven Fitzgerald <eyejabb@gmail.com>
2022-07-17 15:16:24 -0700

    testing weird github error case

--- Completed Solutions/solution0029.js
+++ Completed Solutions/solution0029.js
@@ -1,1 +1,7 @@
-//abcd1234
+// Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+
+
+//e says, 'basically return string with double characters for each parameter input'
+function doubleChar(str) {
+    return [...str].map(s => s.repeat(2)).join('')
+  }
---
+++ Completed Solutions/solution0030.js
@@ -0,0 +1,12 @@
+//We need a function that can transform a number into a string.
+
+// What ways of achieving this do you know?
+
+function numberToString(num) {
+    // Return a string of the number here!
+    return num.toString();
+  }
+
+//eyejabb: `I think the setting a variable and add a Number.toString() method to it has to do with 
+//          function scope, if you set the parameter as a hard value for the .toString method.
+
